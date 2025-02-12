e79bce85cb3aa157acac4431d5c9398ee83a8e83
Heawik <132798471+Heawikk@users.noreply.github.com>
2025-02-12 14:02:47 +0900

    Publicate files (2/12/2025)

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masya Huang™</title>
+    <meta name="description" content="TheMassFox | Main Page">
+    <meta name="theme-color" content="#cd42f4" />
+    <meta name="keywords" content="TheMassFox, MassFox, themassfox, massfox, fox, Fox, Massfox, Furry, furry, Antifurry, antifurry, МассФокс, массфокс, tmfox, TMFOX, тмфокс, тземассфокс, Мася, Масс, мася, масс, vrchat, врчат, furry youtuber, фурри ютубер, фурри блогер, фурри стрим, furry stream, furry live, Лис, Rexouium, rexouium, themassfoxxof">
+    <link rel="apple-touch-icon" sizes="180x180" href="src/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="src/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="src/favicon/favicon-16x16.png">
+    <link rel="icon" href="https://tmfox.ru/src/favicon/favicon.ico" type="image/x-icon">
+    <link rel="manifest" href="src/favicon/site.webmanifest">
+    <meta name="google-site-verification" content="vzdAG3ffJbUSZcYZpJESJVlo1u9UUBi0tZkZIGyZA04" />
+    <meta name="yandex-verification" content="2f390dada995ff61" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Minecraft&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="src/css/styles.css">
+</head>
+<body>
+    <div class="header">
+        <img src="bio/src/main/avatar.png" alt="Avatar" class="avatar">
+        <h1 class="header-text">Masya Huang™</h1>
+    </div>
+    <div class="container">
+        <a href="https://tmfox.ru/bio" target="_blank" ><button class="rounded-button"><i class="fa-solid fa-user"></i> Bio</button><br></a>
+        <a href="https://tmfox.ru/blog" target="_blank" ><button class="rounded-button"><i class="fa-solid fa-bug"></i> Blog</button><br></a>
+        <a href="https://status.tmfox.ru" target="_blank" ><button class="rounded-button"><i class="fa-solid fa-wifi"></i> Services Status</button><br></a>
+        <a href="https://speedtest.tmfox.ru" target="_blank" ><button class="rounded-button"><i class="fa-solid fa-cloud"></i> SpeedTest</button><br></a>
+    </div>
+</body>
+</html>
